a3ffe881a5090566e1b0b33f682a68c2ae0109b5
JamesMinghuaJi <james.nzmh@gmail.com>
2024-03-22 18:43:48 +1300

    fix

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #go build
-GOOS=linux GOARCH=arm64 go build -o jamesjionenz/test ./...
+GOOS=linux GOARCH=arm64 go build -o jamesji86/test ./...
 ## 交叉编译时需要指定CPU架构 GOARCH=amd64 or GOARCH=arm64
 # arm64: 一般为嵌入式CPU架构, 如手机和平板电脑\路由器\NAS 等设备, Mac的新CPU也是ARM架构
 # amd64: 一般为电脑\服务器CPU架构, 主流windows\Liunx服务器均为该型CPU
